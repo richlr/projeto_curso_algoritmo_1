6dcd3a6725dfc359f1f557f874a5ab1a66d27249
richlr <richardlucena@yahoo.com>
2020-04-27 15:56:10 -0300

    Primeiro comitte do código

---
+++ operadores-relacionais.js
@@ -0,0 +1,24 @@
+/*nota01, nota02 :real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   nota01 := 10
+   nota02 := 15
+   
+   se (nota01 = nota02)entao
+      escreval("notas diferentes")
+   senao
+      escreval("notas iguais")
+   fimse
+*/
+
+var nota01, nota02;
+
+   nota01 = 10
+   nota02 = 15
+   
+   if (nota01 != nota02)
+      alert("notas diferentes")
+   else
+      alert("notas iguais")
+   endif
